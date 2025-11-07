592c02b6c9bd1d39d35a034d26605b9f132b2589
jeonjunhee93 <lllllljz@naver.com>
2025-11-07 14:02:59 +0900

    Add files via upload

---
+++ LifeRPG_0.014/src/utils/itemData.js
@@ -0,0 +1,14 @@
+export const equipmentData = [
+  { name: '무딘 칼', type: 'weapon', src: '/무딘칼_일반.png', rarity: '일반' },
+  { name: '루비소드', type: 'weapon', src: '/루비소드_희귀.png', rarity: '희귀' },
+  { name: '파멸의 검', type: 'weapon', src: '/파멸의검_에픽.png', rarity: '에픽' },
+  { name: '아스가르드의 빛', type: 'weapon', src: '/아스가르드의빛_전설.png', rarity: '전설' },
+  { name: '녹슨 철 투구', type: 'helmet', src: '/녹슨 철 투구.png', rarity: '일반' },
+  { name: '용기의 투구', type: 'helmet', src: '/용기의 투구.png', rarity: '희귀' },
+  { name: '검은 달의 투구', type: 'helmet', src: '/검은 달의 투구.png', rarity: '에픽' },
+  { name: '신왕의 면류관', type: 'helmet', src: '/신왕의 면류관.png', rarity: '전설' },
+  { name: '낡은 철 갑옷', type: 'armor', src: '/낡은 철 갑옷.png', rarity: '일반' },
+  { name: '기사단 정예 갑주', type: 'armor', src: '/기사단 정예 갑주.png', rarity: '희귀' },
+  { name: '피의 결의 갑옷', type: 'armor', src: '/피의 결의 갑옷.png', rarity: '에픽' },
+  { name: '신성한 왕의 갑옷', type: 'armor', src: '/신성한 왕의 갑옷.png', rarity: '전설' },
+];
